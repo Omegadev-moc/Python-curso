11e2a165320da206e9d6306798b757b212a33fca
lokpunk1995 <juandavidhernandezayala@gmail.com>
2021-04-01 20:55:19 -0500

    Python

---
+++ prueba.py
@@ -0,0 +1,43 @@
+nombre = input("DIGITE SU IDENTIFICADOR:")
+JC = "JUANITA CASTAÑO"
+EC = "Ernesto Cruz"
+RC = "Romilda de Cruz"
+JA = "Juan arboleda"
+JT = "Jose Trujillo"
+
+
+Hours = "250", "260", "250", "240", "300"
+
+
+Hours_uno = Hours + 10
+Hours_dos = Hours + 12
+
+def es_bisiesto(years: int) -> bool:
+    return years % 4 == 0 and (years % 100 != 0 or years % 400 == 0)
+
+years = int(input("Ingrese el Año: "))
+if (i = 0;>=1900 or years <=1950):
+    print("Empleado con Identificador: " + JC + " es Juanita Castaño y trabajó: " + Hours_dos + " en Febrero del año " + years)
+    print("Empleado con Identificador: " + JC + " es Juanita Castaño y trabajó: " + Hours_dos + " en Febrero del año " + years)
+    print("Empleado con Identificador: " + JC + " es Juanita Castaño y trabajó: " + Hours_dos + " en Febrero del año " + years)
+    print("Empleado con Identificador: " + JC + " es Juanita Castaño y trabajó: " + Hours_dos + " en Febrero del año " + years)
+    print("Empleado con Identificador: " + JC + " es Juanita Castaño y trabajó: " + Hours_dos + " en Febrero del año " + years)
+                else if(
+                 JC !== "JUANITA CASTAÑO"
+                     EC !== "Ernesto Cruz"
+                          RC !== "Romilda de Cruz"
+                            JA !== "Juan arboleda"
+                                 JT !== "Jose Trujillo"
+                                            && !es_bisiesto
+  )
+    print("nombre no encontrado o el año no es bisiesto, intente nuevamente.")
+  else(
+      JC !== "JUANITA CASTAÑO"
+      EC !== "Ernesto Cruz"
+      RC !== "Romilda de Cruz"
+      JA !== "Juan arboleda"
+      JT !== "Jose Trujillo"
+  )
+    print("El identificador no es correcto")
+  if(years !== >=1900 or years <=1950)
+  print("El año no es bisiesto")
